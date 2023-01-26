a10260db54f5997597789f3d58f9d848784fbd8f
mydevth <mydevth@chonburi.biz>
2023-01-26 23:01:15 +0700

    listnameok

--- src/App.js
+++ src/App.js
@@ -3,23 +3,51 @@ import supabase from "./supabase";
 import "./style.css";
 
 function App() {
-  useEffect(() => {
-    async function getFacts() {
-      let query = supabase.from("football").select("*");
+  const [football, setFootball] = useState([]);
 
-      const { data: football, error } = await query
-        .order("id", { ascending: false })
-        .limit(1000);
-      console.log(query);
+  useEffect(function () {
+    async function getName() {
+      const { data: football, error } = await supabase
+        .from("football")
+        .select("*")
+        .order("name", { ascending: true });
+      setFootball(football);
+
+      if (!error) setFootball(football);
+      else alert("There was a problem getting data");
     }
-    getFacts();
+    getName();
   }, []);
 
   return (
     <>
+      <p>this is form App</p>
       <main className="main"></main>
+      <NameList football={football} setFootball={setFootball} />
     </>
   );
 }
 
+function NameList({ football, setFootball }) {
+  if (football.length === 0) return <p className="message"> Loading ..... </p>;
+
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>No.</th>
+            <th>Name</th>
+          </tr>
+          {football.map((el) => (
+            <tr key={el.id}>
+              <th> {el.id} </th>
+              <th> {el.name} </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
 export default App;
