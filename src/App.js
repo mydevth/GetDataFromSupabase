57c58bea4f12443cf3ed575d26afc76b23bb579a
Mydevth <mydevth@chonburi.biz>
2023-01-26 17:42:01 +0700

    start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import supabase from "./supabase";
 import "./style.css";
 
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    async function getFacts() {
+      let query = supabase.from("football").select("*");
+
+      const { data: football, error } = await query
+        .order("id", { ascending: false })
+        .limit(1000);
+      console.log(query);
+    }
+    getFacts();
+  }, []);
+
+  return (
+    <>
+      <main className="main"></main>
+    </>
+  );
 }
 
 export default App;
